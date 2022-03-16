0711b93440b36b07e51cc90b0c559cf589076bcc
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-03-16 08:25:35 +0530

    final

--- shipping.js
+++
@@ -1,8 +0,0 @@
-let subtotal=JSON.parse(localStorage.getItem("subtotal"));
-let discount=JSON.parse(localStorage.getItem("discount"));
-let finalprice=JSON.parse(localStorage.getItem("finalamount"));
-console.log(subtotal);
-
-document.querySelector(".rcdiscount").append(discount[discount.length-1]);
-document.querySelector("#rctotal").append(subtotal[subtotal.length-1]);
-document.querySelector("#rfinaltotal").append(finalprice[finalprice.length-1]);
